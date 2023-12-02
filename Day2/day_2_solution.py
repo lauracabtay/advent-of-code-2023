36eea6aefa2caf0f05ee09a9cb0e9805dad12147
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2023-12-02 16:17:43 +0000

    Refactor solution by creating a ColorCubes class to get cubes by colour

--- Day2/day_2_solution.py
+++ Day2/day_2_solution.py
@@ -1,33 +1,23 @@
 import re
 
 
-def get_blue_cubes(game_input):
-    blue_matches = re.split(' blue', game_input)
-    blue_matches = [blue_match.split(" ", )[-1] for blue_match in blue_matches]
+class ColourCubes:
+    def __init__(self, colour):
+        self.colour = colour
 
-    if not blue_matches[-1].isdigit():
-        blue_matches.pop()
+    def get_colour_cubes(self, game_input):
+        colour_sections = re.split(" " + self.colour, game_input)
+        colour_matches = [
+            int(colour_section.split(" ")[-1]) for colour_section in colour_sections
+            if colour_section.split(" ")[-1].isdigit()
+        ]
+        return colour_matches
 
-    return blue_matches
-
-
-def get_red_cubes(game_input):
-    red_matches = re.split(' red', game_input)
-    red_matches = [red_match.split(" ", )[-1] for red_match in red_matches]
-
-    if not red_matches[-1].isdigit():
-        red_matches.pop()
-
-    return red_matches
-
-
-def get_green_cubes(game_input):
-    green_matches = re.split(' green', game_input)
-    green_matches = [green_match.split(" ", )[-1] for green_match in green_matches]
-
-    if not green_matches[-1].isdigit():
-        green_matches.pop()
-    return green_matches
+    def get_impossible_cubes(self, game_input, max_limit):
+        return [
+            int(colour_match) for colour_match in self.get_colour_cubes(game_input)
+            if int(colour_match) > max_limit
+        ]
 
 
 def find_games_total(input):
@@ -36,16 +26,11 @@ def find_games_total(input):
     for game in input:
         game_number = int(game.split("Game ", 1)[1].split(":")[0])
 
-        blue_matches = get_blue_cubes(game)
-        blue_matches = [int(blue_match) for blue_match in blue_matches if int(blue_match) > 14]
+        impossible_blue = ColourCubes('blue').get_impossible_cubes(game, 14)
+        impossible_red = ColourCubes('red').get_impossible_cubes(game, 12)
+        impossible_green = ColourCubes('green').get_impossible_cubes(game, 13)
 
-        red_matches = get_red_cubes(game)
-        red_matches = [int(red_match) for red_match in red_matches if int(red_match) > 12]
-
-        green_matches = get_green_cubes(game)
-        green_matches = [int(green_match) for green_match in green_matches if int(green_match) > 13]
-
-        if len(blue_matches) == 0 and len(red_matches) == 0 and len(green_matches) == 0:
+        if len(impossible_blue) + len(impossible_red) + len(impossible_green) == 0:
             game_total += game_number
 
     return game_total
@@ -55,17 +40,14 @@ def find_games_power(input):
     game_powers_sum = 0
 
     for game in input:
-        blue_matches = get_blue_cubes(game)
-        blue_matches = [int(blue_match) for blue_match in blue_matches]
-        min_blue = max(blue_matches) if blue_matches else 1
+        blue_cubes = ColourCubes('blue').get_colour_cubes(game)
+        min_blue = max(blue_cubes) if blue_cubes else 1
 
-        red_matches = get_red_cubes(game)
-        red_matches = [int(red_match) for red_match in red_matches]
-        min_red = max(red_matches) if red_matches else 1
+        red_cubes = ColourCubes('red').get_colour_cubes(game)
+        min_red = max(red_cubes) if red_cubes else 1
 
-        green_matches = get_green_cubes(game)
-        green_matches = [int(green_match) for green_match in green_matches]
-        min_green = max(green_matches) if green_matches else 1
+        green_cubes = ColourCubes('green').get_colour_cubes(game)
+        min_green = max(green_cubes) if green_cubes else 1
 
         game_power = min_blue * min_red * min_green
         game_powers_sum += game_power
@@ -84,6 +66,7 @@ def output_solution_part_2():
         games = input_file.read().split('\n')
 
     print(find_games_power(games))
+
 
 output_solution_part_1()
 output_solution_part_2()
--- Day2/test_day_2_solution.py
+++ Day2/test_day_2_solution.py
@@ -1,46 +1,54 @@
 import unittest
-from day_2_solution import find_games_total, find_games_power, get_blue_cubes, get_red_cubes, get_green_cubes
+from day_2_solution import find_games_total, find_games_power, ColourCubes
+
+
+class TestColorCubes(unittest.TestCase):
+    def setUp(self):
+        self.color_cubes_instance = ColourCubes("blue")
+
+    def test_get_colour_cubes(self):
+        no_blue = 'Game 3: 8 green, 20 red; 4 red, 13 green; 5 green, 1 red'
+        one_blue = 'Game 3: 6 blue, 20 red; 4 red, 13 green; 5 green, 1 red'
+        multiple_blue = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red; 5 green'
+
+        result = self.color_cubes_instance.get_colour_cubes(no_blue)
+        assert result == []
+
+        result = self.color_cubes_instance.get_colour_cubes(one_blue)
+        assert result == [6]
+
+        result = self.color_cubes_instance.get_colour_cubes(multiple_blue)
+        assert result == [6, 5]
+
+    def test_get_impossible_cubes(self):
+        no_blue = 'Game 3: 8 green, 20 red; 4 red, 13 green; 5 green, 1 red'
+        one_blue = 'Game 3: 8 green, 15 blue, 6 blue, 1 red'
+        multiple_blue = 'Game 3: 8 green, 15 blue; 17 blue, 4 red; 5 blue'
+
+        result = self.color_cubes_instance.get_impossible_cubes(no_blue, 14)
+        assert result == []
+
+        result = self.color_cubes_instance.get_impossible_cubes(one_blue, 14)
+        assert result == [15]
+
+        result = self.color_cubes_instance.get_impossible_cubes(multiple_blue, 14)
+        assert result == [15, 17]
+
 
 class TestDay2Solution(unittest.TestCase):
-    def test_get_blue_cubes(self):
-        test_input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
-        result = get_blue_cubes(test_input)
-
-        assert result == ['3', '6']
-
-    def test_get_red_cubes(self):
-        test_input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
-        result = get_red_cubes(test_input)
-
-        assert result == ['4', '1']
-
-    def test_get_green_cubes(self):
-        test_input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
-        result = get_green_cubes(test_input)
-
-        assert result == ['2', '2']
-
-    def test_find_games_total(self):
-        test_input = [
+    def setUp(self):
+        self.test_input = [
             'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
             'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
             'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
             'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
             'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
         ]
-        result = find_games_total(test_input)
 
+    def test_find_games_total(self):
+        result = find_games_total(self.test_input)
         assert result == 8
 
     def test_find_games_power(self):
-        test_input = [
-            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
-        ]
-        result = find_games_power(test_input)
-
+        result = find_games_power(self.test_input)
         assert result == 2286
-
