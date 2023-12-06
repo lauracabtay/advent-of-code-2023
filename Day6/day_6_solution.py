bbde69cf4ee7f7f1d6ad1e316b6fd870d93cb862
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2023-12-06 07:27:26 +0000

    Refactor part 1

--- Day6/day_6_solution.py
+++ Day6/day_6_solution.py
@@ -2,24 +2,27 @@ def find_record_breaking_times(file_name):
     race_times = [int(x) for x in file_name[0].split() if x.isdigit()]
     record_times = [int(x) for x in file_name[1].split() if x.isdigit()]
 
-    wins = []
+    wins = 1
 
     for index, race in enumerate(race_times):
-        win_count = 0
+        first_winning_time = 0
+        last_winning_time = 0
+
         for i in range(1, race):
             distance = (race - i) * i
-
             if distance > record_times[index]:
-                win_count += 1
+                first_winning_time = i
+                break
 
-        wins.append(win_count)
+        for j in range(race, 0, -1):
+            distance = (race - j) * j
+            if distance > record_times[index]:
+                last_winning_time = j
+                break
 
-    result = 1
+        wins *= last_winning_time - first_winning_time + 1
 
-    for win in wins:
-        result *= win
-
-    return result
+    return wins
 
 
 with open('input.txt', 'r') as input_file:
