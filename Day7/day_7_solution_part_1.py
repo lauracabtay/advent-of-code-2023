d68db2c7d59c7274b9ba67374bd0b7dee1e8638b
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2023-12-07 15:10:29 +0000

    Implement solution for Day7

---
+++ Day7/day_7_solution_part_1.py
@@ -0,0 +1,108 @@
+import pprint
+
+import numpy as np
+from collections import Counter
+
+HIGH_TO_LOW_CARD_VALUE = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+
+def custom_sort_key(item):
+    return [HIGH_TO_LOW_CARD_VALUE.index(char) for char in item]
+
+
+def sort_array(hands):
+    return sorted(hands, key=custom_sort_key, reverse=True)
+
+
+def is_five_of_a_kind(counter):
+    return len(counter) == 1
+
+
+def is_four_of_a_kind(counter):
+    return len(counter) == 2 and max(counter.values()) == 4
+
+
+def is_full_house(counter):
+    return len(counter) == 2 and not max(counter.values()) == 4
+
+
+def is_three_of_a_kind(counter):
+    return len(counter) == 3 and max(counter.values()) == 3
+
+
+def is_two_pair(counter):
+    return len(counter) == 3 and not max(counter.values()) == 3
+
+
+def is_one_pair(counter):
+    return len(counter) == 4
+
+
+def is_high_card(counter):
+    return len(counter) == 5
+
+
+with open('input.txt', 'r') as input_file:
+    file = input_file.read().split('\n')
+    hands = [x.split(' ')[0] for x in file]
+    bids = [x.split(' ')[1] for x in file]
+
+    games = []
+
+    hand_types = {
+        "five_of_a_kind": [],
+        "four_of_a_kind": [],
+        "full_house": [],
+        "three_of_a_kind": [],
+        "two_pair": [],
+        "one_pair": [],
+        "high_card": [],
+    }
+
+    for hand, bid in zip(hands, bids):
+        player = {hand: int(bid)}
+        games.append(player)
+        counter = Counter(hand)
+
+        if is_five_of_a_kind(counter):
+            hand_types["five_of_a_kind"].append(hand)
+        elif is_four_of_a_kind(counter):
+            hand_types["four_of_a_kind"].append(hand)
+        elif is_full_house(counter):
+            hand_types["full_house"].append(hand)
+        elif is_three_of_a_kind(counter):
+            hand_types["three_of_a_kind"].append(hand)
+        elif is_two_pair(counter):
+            hand_types["two_pair"].append(hand)
+        elif is_one_pair(counter):
+            hand_types["one_pair"].append(hand)
+        elif is_high_card(counter):
+            hand_types["high_card"].append(hand)
+
+    sorted_hands = {}
+
+    for hand_type, hands in hand_types.items():
+        sorted_hands[hand_type] = sort_array(hands)
+
+    new_array = np.concatenate(
+        (
+            sorted_hands['high_card'],
+            sorted_hands['one_pair'],
+            sorted_hands['two_pair'],
+            sorted_hands['three_of_a_kind'],
+            sorted_hands['full_house'],
+            sorted_hands['four_of_a_kind'],
+            sorted_hands['five_of_a_kind'],
+        )
+    )
+
+    total_winnings = 0
+
+    for index, card in enumerate(new_array):
+        for game in games:
+            if card in game:
+                card_value = game[card]
+                total_winnings += card_value * (index + 1)
+                break
+
+    print(total_winnings)
